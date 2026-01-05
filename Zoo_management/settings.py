65099ac427bc4baac432e1ee189e1e32abbbcfb8
Ociuz Academy <ociuzacademy@gmail.com>
2026-01-05 15:59:45 +0530

    Remove email host user and password
    Clear email credentials for security reasons.

--- Zoo_management/settings.py
+++ Zoo_management/settings.py
@@ -31,8 +31,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'demoemail939' 
-EMAIL_HOST_PASSWORD = 'odubbnmagjojrajd'
+EMAIL_HOST_USER = '' 
+EMAIL_HOST_PASSWORD = ''
 
 
 # Application definition
